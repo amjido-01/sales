89c80d6992a0e9a971a20252b2f59f647350681a
amjido-01 <youndsadeeq10@gmail.com>
2025-05-22 10:27:52 +0100

    set async storage to persist user data

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth"; 
+import AsyncStorage from "@react-native-async-storage/async-storage";   
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
+const auth = initializeAuth(app, {   persistence: getReactNativePersistence(AsyncStorage) });
 const analytics = getAnalytics(app);
 // ✅ Export them
-export { app, auth, analytics };
+export { auth, analytics };
